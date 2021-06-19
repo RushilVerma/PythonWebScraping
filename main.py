d2dc90f8df2ceccae91387ce05720c6174ba9851
Rushil Verma <44629500+RushilVerma@users.noreply.github.com>
2021-06-19 22:32:00 +0530

    url and headers and some imports

--- main.py
+++ main.py
@@ -1,3 +1,23 @@
 #Code for WebScrapping 
 #Author: Rushil Verma
 #Description : For Nemish Technology Task
+
+from typing import Text
+import requests
+from bs4 import BeautifulSoup
+
+url = "https://www.propertiesguru.com/residential-search/2bhk-residential_apartment_flat-for-sale-in-new_delhi"
+headers = {"User-Agent" : "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.106 Safari/537.36"}
+
+req = requests.get(url, headers= headers)
+
+#req.status_code #200 is good
+
+soup = BeautifulSoup(req.content, "html.parser",features = 'lxml')
+
+#print(soup.get_text()) #to get only text
+#print(soup.prettify()) #to get html src in nicely formated form
+
+info = soup.find('span'{'id': 'priceblock_ourprice'}).text
+
+print(info)
